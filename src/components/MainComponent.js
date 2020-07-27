864b31e67ca1a4081fe055ef5833ef63944a4871
Shreyasi Sinha <shreyasi_sinha@hotmail.com>
2020-07-27 12:45:57 +0530

    react route --linked

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,32 +1,45 @@
 //pseudo container component making the other two presentational
 import React, { Component } from 'react';
+import Home from './HomeComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent' ;
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
       super(props);
       this.state = {
         dishes: DISHES,
-        selectedDish: null
+
       };
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId});
-    }
+    // onDishSelect(dishId) {
+    //     this.setState({ selectedDish: dishId});
+    // }
    
     render(){
+        const HomePage=() => {
+            return(
+                <Home />
+            );
+        }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} 
+        {/* <Menu dishes={this.state.dishes} 
         onClick={(dishId) => this.onDishSelect(dishId)}/>
         <DishDetail 
-            dish={this.state.dishes.filter((dish) => dish.id ===this.state.selectedDish)[0]} />
+            dish={this.state.dishes.filter((dish) => dish.id ===this.state.selectedDish)[0]} /> */}
+            <Switch>
+                <Route path="/home" component={HomePage} />
+                <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes} />} />
+                <Redirect to="/home" />
+            </Switch>
+            
             <Footer />
       </div>
     );
