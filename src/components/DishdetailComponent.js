5d8e2fef40dfd8d7bf81ffeace8f096294fed7ec
Shreyasi Sinha <shreyasi_sinha@hotmail.com>
2020-07-29 22:52:08 +0530

    second set of animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import  {Link} from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import {Loading } from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 const maxLength = (len) => (val)=> !(val) || (val.length <= len);
 const minLength = (len) => (val)=> !(val) || (val.length >= len);
 
@@ -86,6 +87,10 @@ class CommentForm extends Component{
 
 function RenderDish({dish}) {
     return(
+        <FadeTransform in 
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
         <Card>
             <CardImg src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
@@ -93,6 +98,7 @@ function RenderDish({dish}) {
             <CardText>{dish.description}</CardText>
             </CardBody>
         </Card>
+        </FadeTransform>
     );
 
 }
@@ -104,7 +110,7 @@ function RenderDish({dish}) {
             }
            var menus=comments.map((commenti) =>{
                 return(
-                    
+                    <Fade in>
                     <li key={commenti.id} style={{fontSize: 18}}>
                         <div><p>{commenti.comment}</p></div>
                          <div><p>--{commenti.author}, {new Intl.DateTimeFormat('en-US', {
@@ -113,14 +119,17 @@ function RenderDish({dish}) {
                             day: '2-digit'
                         }).format(new Date(Date.parse(commenti.date)))}</p></div>   
                       </li>
+                      </Fade>
                 );
            });
            return(
                <div>
                    <h4>Comments</h4>
                    <ul className='list-unstyled'>
+                       <Stagger in>
                     {menus}
                     <CommentForm dishId={dishId} postComment={postComment}/>
+                    </Stagger>
                 </ul>
                 </div>
            );
